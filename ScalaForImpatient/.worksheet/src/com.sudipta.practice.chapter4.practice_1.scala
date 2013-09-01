60652c55d4dd9730565ffbbc385e58805a3b30fd
Sudipta Deb <sudipta.deb@gmail.com>
2013-09-01 20:50:39 +0530

    Delete com.sudipta.practice.chapter4.practice_1.scala

--- ScalaForImpatient/.worksheet/src/com.sudipta.practice.chapter4.practice_1.scala
+++
@@ -1,5 +0,0 @@
-package com.sudipta.practice.chapter4
-
-object practice_1 {;import org.scalaide.worksheet.runtime.library.WorksheetSupport._; def main(args: Array[String])=$execute{;$skip(102); 
-  println("Welcome to the Scala worksheet")}
-}
