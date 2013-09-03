f0c3ef99350f6922078cddfda12ba3886b62cba6
Sudipta Deb <sudipta.deb@gmail.com>
2013-09-03 21:27:03 +0530

    Testing Getters and Setters

--- ScalaForImpatient/src/com/sudipta/practice/chapter5/Person.scala
+++ ScalaForImpatient/src/com/sudipta/practice/chapter5/Person.scala
@@ -1,11 +1,37 @@
 package com.sudipta.practice.chapter5
 
 class Person {
-	var age = 0
+	//var age = 0
+	private var privateAge = 0
+	private val privateName = "Sudipta Deb"
+	
+	def age = privateAge
+	def age_=(newAge : Int) = {
+	  if(newAge > privateAge)
+	    privateAge = newAge
+	}
+	
+	def name = privateName
+	
+	//Since privateName is a val, so only Getter is there. No Setter is possible
+	
+	/*
+	 * def name_=(newName: String) = {
+	 * privateName = newName
+	 *	}
+	*/
 }
 
 object PersonTest extends App {
   val sudipta = new Person
   sudipta.age = 20
-  println(sudipta.age)
+  println("Initial Age: " + sudipta.age)
+  
+  sudipta.age = 18
+  println("Trying to get younger: " + sudipta.age)
+  
+  sudipta.age = 29
+  println("My bad luck, only older is possible : " + sudipta.age)
+  
+  println("My Name is: " + sudipta.name)
 }
